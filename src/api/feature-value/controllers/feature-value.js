e17b3b8ae7dde98ef22a97e56ae05c56dbed2de1
Khushi <khushiv@geekyants.com>
2025-05-22 16:00:58 +0530

    schema changes for pricing and removed unused collection type for pricing

--- src/api/feature-value/controllers/feature-value.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * feature-value controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::feature-value.feature-value');
