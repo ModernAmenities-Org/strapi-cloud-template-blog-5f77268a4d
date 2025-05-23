8c2f6abc2ca0fa7d30451bfbd660b1cd3fd45518
Khushi <khushiv@geekyants.com>
2025-05-23 11:39:21 +0530

    fix.

---
+++ config/server.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  apiTokenSalt: env('API_TOKEN_SALT'),
+  adminJwtSecret: env('ADMIN_JWT_SECRET'),
+  jwt: {
+    secret: env('JWT_SECRET'),
+  },
+  transferTokenSalt: env('TRANSFER_TOKEN_SALT'),
+});
