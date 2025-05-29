2b50e6c14b83c87c2433af182ab5c6020ad4830a
Ruchika Gupta <ruchikagupta@Ruchikas-MacBook-Pro.local>
2025-05-29 13:34:08 +0530

    added graphql for types

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+// config/plugins.js
+module.exports = () => ({
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true,
+      depthLimit: 10,
+      amountLimit: 100,
+      apolloServer: {
+        tracing: false,
+        introspection: true,
+      },
+    },
+  },
+});
