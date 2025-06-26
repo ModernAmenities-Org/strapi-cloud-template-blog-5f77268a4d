c23cb678796c6ab499d3e5eeaa033c4b8159ee01
Th Nanganba Singha <thnanganbasingha@Ths-MacBook-Pro.local>
2025-06-26 16:35:30 +0530

    feature page segregration

---
+++ src/api/feature-vend-route/controllers/feature-vend-route.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * feature-vend-route controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::feature-vend-route.feature-vend-route');
---
+++ src/api/feature-vend-route/routes/feature-vend-route.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * feature-vend-route router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::feature-vend-route.feature-vend-route');
